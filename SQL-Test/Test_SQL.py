fe23b372bf6b2be0f60c9606bea49189afbf644f
Michael Levan <mlevan1992@gmail.com>
2021-03-03 08:52:01 -0500

    Python code for DB testing

---
+++ SQL-Test/Test_SQL.py
@@ -0,0 +1,38 @@
+import mysql.connector
+from mysql.connector import errorcode
+import sys
+
+
+def testdb():
+    try:
+        connect = mysql.connector.connect(
+            user=user,
+            password=password,
+            host=host,
+            database=database
+        )
+
+    except mysql.connector.Error as error:
+        if error.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Access Denied for this user")
+
+        else:
+            cursor = connect.cursor()
+
+            cursor.execute("SELECT FirstName FROM users")
+            result = cursor.fetchall()
+
+            for query in result:
+                print(query)
+
+            connect.close()
+            
+
+user = sys.argv[1]
+password = sys.argv[2]
+host = sys.argv[3]
+database = sys.argv[4]
+
+
+if __name__ == '__main__':
+    testdb()
